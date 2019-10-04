ec4536dcfab4bfc3b0d29d790ce2205a6d7379a5
DiegoYance <diegoyance@unmsm.edu.pe>
2019-10-04 13:02:27 -0500

    Implementacionde menus desplegables

--- tpl/header.php
+++ tpl/header.php
@@ -1,26 +1,90 @@
 
-<header>
+<nav role="navigation">
 
   <div class="h-row-1">
     <div class="container">
       <div class="row align-items-center">
         <div class="col-12 col-md-4">
-          <a href=""><i class="fas fa-book"></i>&nbsp;&nbsp; Mis cursos &nbsp; <i class="fas fa-caret-right"></i></a>
+          
+          <div class="navbar">
+              <div class="container-fluid navbar-inner">
+                  <div class="row-fluid">
+                      <ul class="nav">
+                          <li class="dropdown">
+                              <a class="dropdown-toggle" data-toggle="dropdown" href="inicio.php"><i class="fas fa-book"></i>&nbsp;&nbsp; Mis cursos</a>
+                              <ul class="dropdown-menu">
+                                  <li>
+                                      <a class="dropdown-item" href="inicio.php"><em><span aria-hidden="true" class="fa fa-home"></span> Area Personal</em></a>
+                                  </li>
+                                  <li>
+                                      <a class="dropdown-item" href="curso.php"><em><span aria-hidden="true" class="fa fa-graduation-cap"></span>Metodologia de la investigacion</em></a>
+                                  </li>
+                                  <li>
+                                      <a class="dropdown-item" href="curso.php"><em><span aria-hidden="true" class="fa fa-graduation-cap"></span>Interaccion hombre computador</em></a>
+                                  </li>
+                                  <li>
+                                      <a class="dropdown-item" href="curso.php"><em><span aria-hidden="true" class="fa fa-graduation-cap"></span>Gerencia de proyectos</em></a>
+                                  </li>
+                                  <li>
+                                      <a class="dropdown-item" href="curso.php"><em><span aria-hidden="true" class="fa fa-graduation-cap"></span>Gestion de configuracion y mantenimiento</em></a>
+                                  </li>
+                                  <li>
+                                      <a class="dropdown-item" href="curso.php"><em><span aria-hidden="true" class="fa fa-graduation-cap"></span>Redes y transmision de datos</em></a>
+                                  </li>
+                                  <li>
+                                      <a class="dropdown-item" href="curso.php"><em><span aria-hidden="true" class="fa fa-graduation-cap"></span>Testing de software</em></a>
+                                  </li>
+                              </ul>
+                          </li>
+                      </ul>                        
+                  </div>   
+              </div>                                           
+            </div>
         </div>
         <div class="col-12 offset-md-4 col-md-4 offset-lg-5 col-lg-3">
           <div class="row justify-content-center align-items-center">
             <a href="chat.php"><i class="fas fa-envelope"></i> </a>
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-            <a href=""><i class="fas fa-user-circle"></i></a>
-            &nbsp; &nbsp; <a href="">Diego Yance</a> &nbsp; &nbsp;
-            <a href=""><i class="fas fa-caret-right"></i></a>
+            <div class="navbar">
+              <div class="container-fluid navbar-inner">
+                  <div class="row-fluid">
+                      <ul class="nav">
+                          <li class="dropdown">
+                              <a class="dropdown-toggle" data-toggle="dropdown" href="inicio.php">
+                                Diego Yance 
+                              </a>
+                              <ul class="dropdown-menu">
+                                  <li>
+                                      <a class="dropdown-item" href="inicio.php"><em><span aria-hidden="true" class="fa fa-user"></span> Diego Alberto Yance Arqque</em></a>
+                                  </li>
+                                  
+                                  <hr class="sep">
+                                  <li>
+                                      <a class="dropdown-item" href="editar_perfil.php"><em><span aria-hidden="true" class="fa fa-info-circle"></span> Editar perfil</em></a>
+                                  </li>
+                                  <li>
+                                      <a class="dropdown-item" href="chat.php"><em><span aria-hidden="true" class="fa fa-envelope"></span> Mensajes</em></a>
+                                  </li>
+                                  <li>
+                                      <a class="dropdown-item" href="calificacion.php"><em><span aria-hidden="true" class="fa fa-star"></span> Mis calificaciones</em></a>
+                                  </li>
+                                  <hr class="sep">
+                                  <li>
+                                      <a class="dropdown-item" href="index.php"><em><span aria-hidden="true" class="fa fa-sign-out-alt"></span> Salir</em></a>
+                                  </li>
+                              </ul>
+                          </li>
+                      </ul>                        
+                  </div>   
+              </div>                                           
+            </div>    
           </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="h-row-2">
+  <div class="h-row-2" style="margin-top:50px">
     <div class="container">
       <div class="row align-items-center">
         <div class="h-logo col-12 col-md-4">
@@ -40,7 +104,7 @@
     </div>
   </div>
 
-</header>
+</nav>
 
 
 
